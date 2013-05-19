f589c5db4e9cc24a47370265b532008a7b549d2f
Zach Serafini <serafini.8@osu.edu>
2013-05-19 13:20:58 -0400

    Added Kill button on Drop page to save phone from wasting data whhen done testing

--- Android/assets/www/js/index.js
+++ Android/assets/www/js/index.js
@@ -5,27 +5,35 @@ $(document).ready(function() {
 	
 	//Keep updating GPS data
 	var watchPosition = navigator.geolocation.watchPosition( 
-			function(position){lat = position.coords.latitude; lon = position.coords.longitude;} 
-			,function(){alert('GPS FAIL!')} 
-			,{frequency:5000,maximumAge: 30000, timeout: 30000, enableHighAccuracy:true}); 
+		function(position){lat = position.coords.latitude; lon = position.coords.longitude;} 
+		,function(){alert('GPS FAIL!')} 
+		,{frequency:5000,maximumAge: 30000, timeout: 30000, enableHighAccuracy:true}); 
 	
 	
 	$("#log").click(function() 
 	{
 		window.location.href = "log.html";
-	});  
+	}); 
+	
+	$("#kill").click(function() 
+	{
+		navigator.geolocation.clearWatch(watchPosition);
+		navigator.app.exitApp();
+	}); 
     
 	//Start a new message request when drop button is clicked
     $("#newdrop").click(function() 
     {
     	//Send new request to server
-    	var request = $.ajax({
+    	var request = $.ajax(
+    	{
             url: "http://ec2-23-22-121-122.compute-1.amazonaws.com/MessageDrop/CreateMessage.php",
             type: "post",
             data: {message: $('input[id=message]').val(), lon: lon, lat: lat}
         });
         
-        request.done(function (response, textStatus, jqXHR){
+        request.done(function (response, textStatus, jqXHR)
+        {
         	alert('Message Dropped!');
         });
     });
--- Android/assets/www/js/log.js
+++ Android/assets/www/js/log.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() { 
 	
 	var lat = 0.0;
@@ -15,23 +14,23 @@ $(document).ready(function() {
 		
 		//Ajax request to get message data from db
 		var request = $.ajax(
-			    {
-			        url: "http://ec2-23-22-121-122.compute-1.amazonaws.com/MessageDrop/Log.php",
-			        type: "post",
-			        data: {lon: lon, lat: lat}
-			        
-			    });
+		{
+			url: "http://ec2-23-22-121-122.compute-1.amazonaws.com/MessageDrop/Log.php",
+			type: "post",
+			data: {lon: lon, lat: lat}        
+		});
 			    
-			    request.done(function (response, textStatus, jqXHR)
-			    {
-			    	//Clear list for new values
-			    	$("#list").html('');
-			    	//Write new results to log window
-			    	response = $.parseJSON(response);
-			    	$.each(response, function(i, item) {
-			    	    $("#list").append('<p><em>'+item['message']+'</em><br>Distance:'+item['distance']+' mi.</p><hr>');
-			    	});
-			    });
+		request.done(function (response, textStatus, jqXHR)
+		{
+			//Clear list for new values
+			$("#list").html('');
+			//Write new results to log window
+			response = $.parseJSON(response);
+			$.each(response, function(i, item) 
+			{
+				$("#list").append('<p><em>'+item['message']+'</em><br>Distance:'+item['distance']+' mi.</p><hr>');
+			});
+		});
 	}
 	
     
@@ -39,9 +38,7 @@ $(document).ready(function() {
     $("#drop").click(function() 
     {
     	window.location.href = "index.html";
-  	});   
-
-    
+  	});       
 }); 
 
 
