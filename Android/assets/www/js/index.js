fe77e93362ca1be3d6709f45cd2a7efd4664db18
Zach Serafini <serafini.8@osu.edu>
2013-05-19 12:56:17 -0400

    Got messages to log cords and return distance from drop location

--- Android/assets/www/js/index.js
+++ Android/assets/www/js/index.js
@@ -1,11 +1,32 @@
 $(document).ready(function() { 
 	
-    $('#newdrop').ajaxForm(function() { 
-        alert("Message Dropped!"); 
-        $
-    }); 
+	var lat = 0.0;
+	var lon = 0.0;
+	
+	//Keep updating GPS data
+	var watchPosition = navigator.geolocation.watchPosition( 
+			function(position){lat = position.coords.latitude; lon = position.coords.longitude;} 
+			,function(){alert('GPS FAIL!')} 
+			,{frequency:5000,maximumAge: 30000, timeout: 30000, enableHighAccuracy:true}); 
+	
+	
+	$("#log").click(function() 
+	{
+		window.location.href = "log.html";
+	});  
     
-    $("#log").click(function() {
-    	window.location.href = "log.html";
-  	});
+	//Start a new message request when drop button is clicked
+    $("#newdrop").click(function() 
+    {
+    	//Send new request to server
+    	var request = $.ajax({
+            url: "http://ec2-23-22-121-122.compute-1.amazonaws.com/MessageDrop/CreateMessage.php",
+            type: "post",
+            data: {message: $('input[id=message]').val(), lon: lon, lat: lat}
+        });
+        
+        request.done(function (response, textStatus, jqXHR){
+        	alert('Message Dropped!');
+        });
+    });
 }); 
--- Android/assets/www/js/log.js
+++ Android/assets/www/js/log.js
@@ -1,25 +1,43 @@
 
 $(document).ready(function() { 
 	
-	var $form = $(this);
-	var serializedData = $form.serialize();
+	var lat = 0.0;
+	var lon = 0.0;
 	
-    var request = $.ajax({
-        url: "http://ec2-23-22-121-122.compute-1.amazonaws.com/MessageDrop/Log.php",
-        type: "post",
-        data: serializedData
-    });
+	//Keep updating GPS data for log
+	var watchPosition = navigator.geolocation.watchPosition( 
+			function(position){lat = position.coords.latitude; lon = position.coords.longitude; onSuccess();} 
+			,function(){alert('GPS FAIL!')} 
+			,{frequency:5000,maximumAge: 30000, timeout: 30000, enableHighAccuracy:true}); 
+	
+	function onSuccess()
+	{
+		
+		//Ajax request to get message data from db
+		var request = $.ajax(
+			    {
+			        url: "http://ec2-23-22-121-122.compute-1.amazonaws.com/MessageDrop/Log.php",
+			        type: "post",
+			        data: {lon: lon, lat: lat}
+			        
+			    });
+			    
+			    request.done(function (response, textStatus, jqXHR)
+			    {
+			    	//Clear list for new values
+			    	$("#list").html('');
+			    	//Write new results to log window
+			    	response = $.parseJSON(response);
+			    	$.each(response, function(i, item) {
+			    	    $("#list").append('<p><em>'+item['message']+'</em><br>Distance:'+item['distance']+' mi.</p><hr>');
+			    	});
+			    });
+	}
+	
     
-    request.done(function (response, textStatus, jqXHR){
-        // log a message to the console
-    	response = $.parseJSON(response);
-    	$.each(response, function(i, item) {
-    	    //alert(item['message']);
-    	    $("#list").append('<p>'+item['message']+'</p>');
-    	});
-    });
 	
-    $("#drop").click(function() {
+    $("#drop").click(function() 
+    {
     	window.location.href = "index.html";
   	});   
 
